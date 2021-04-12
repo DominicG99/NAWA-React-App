1b8b5814d20878001fc61842c5f6eec1d0f84304
James <jamesyoung.n@gmail.com>
2021-04-12 12:21:51 -0700

    Temp upload
    to start liveshare

--- client/src/components/ProjectHeader.js
+++ client/src/components/ProjectHeader.js
@@ -4,26 +4,32 @@ function ProjectHeader() {
   return (
     <div
       style={{
-        background: "url(../../images/image1.jpg)",
+        background: "url(../../images/aboutusroad.jpg)",
         padding: "15rem",
       }}
     >
       <div>
-        <h1
-          style={{ marginBottom: "3%", marginTop: "2%", textAlign: "center" }}
-        >
-          <strong>Not Just A Weather App</strong>
+        <h1 className = "mainHeading">
+          Not Just a Weather App 
         </h1>
 
-        <h5 className="subHeading">Team 20</h5>
-        <h5 className="subHeading">
+        <h5 className = "classHeading">
           CS 426 Senior Project in Computer Science
         </h5>
-        <h5 className="subHeading">Spring 2021</h5>
-        <h5 className="subHeading">
+
+        <h5 className="schoolHeading">
           University of Nevada, Reno, CSE Department
         </h5>
-      </div>
+
+        <h5 className="semesterHeading">
+        Spring 2021
+        </h5>
+        
+        <h5 className="teamHeading">
+        Team #20
+        </h5>
+        
+        </div>
     </div>
   );
 }
